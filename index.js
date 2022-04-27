70a1d5629c15eb55aad92b2fb20e9d3ae1482b5f
ricardo <rmontecinosmolina@gmail.com>
2022-04-27 16:43:40 -0300

    function renderpost

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
+let postArray = []
+
+function renderPosts(){
+    let html = ""
+    for(let post of postArray){
+        html += "<h3>" + post.title + "</h3><p>" + post.body + "</p><hr/>"
+    }
+    document.getElementById("blog-list").innerHTML = html
+}
+
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        const postArr = data.slice(0, 5)
-        let html = ""
-        for(let post of postArr){
-            html += "<h3>" + post.title + "</h3><p>" + post.body + "</p>"
-        }
-        document.getElementById("blog-list").innerHTML= html
+        postArray = data.slice(0, 5)
+        renderPosts()
     })
 
 document.getElementById("new-post").addEventListener("submit", function(e){
-    e.preventDefault
+    e.preventDefault()
     const postTitle = document.getElementById("post-title").value
     const postBody = document.getElementById("post-body").value
     const data = {
         title: postTitle,
         body: postBody
     }
+
+    const options = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+        .then(res => res.json())
+        .then(post => {
+            console.log(post)
+            console.log("postArray: " + postArray)
+            postArray.unshift(post)
+            renderPosts()
+        })
 })
